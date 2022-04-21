4046a6134e4a4ec3b9a4f5477ea6e0c895fe79ea
Elena Zobak <elenazobak@gmail.com>
2022-04-21 12:36:06 -0400

    history styling

--- components/Game.js
+++ components/Game.js
@@ -42,14 +42,12 @@ export default function App({ route, navigation }) {
   });
   const [history, setHistory] = useState([]);
 
-  
-
   //Get the random number
   const fetchNumber = async () => {
     const { data: random } = await axios.get(
       `https://www.random.org/integers/?num=${CELL_COUNT}&min=0&max=7&col=4&base=10&format=plain&rnd=new`
     );
-    
+
     setRandomNumber(random.replace(/\s/g, ""));
   };
 
@@ -58,14 +56,13 @@ export default function App({ route, navigation }) {
     if (!randomNumber) {
       setRandomNumber("1234");
     }
-    
   }, []);
 
-  //Check submission
+  //Checking submission
   const checkSubmitUpdateState = (tempUserInput, randomNumber) => {
     let numberObj = {};
     for (let i = 0; i < randomNumber.length; i++) {
-      //Correct location (deletes from input array)
+      //Counts the well positioned numbers, replaces them with null in the tempInput array
       if (randomNumber[i] === tempUserInput[i]) {
         setNumAndLocation((numAndLocation) => numAndLocation + 1);
         tempUserInput[i] = null;
@@ -76,7 +73,7 @@ export default function App({ route, navigation }) {
         } else numberObj[randomNumber[i]] = 1;
       }
     }
-    //Checks correct numbers with wrong location
+    //Correct numbers with wrong location
     for (let j = 0; j < tempUserInput.length; j++) {
       if (numberObj[tempUserInput[j]]) {
         setCorrectNum((correctNum) => correctNum + 1);
@@ -85,16 +82,18 @@ export default function App({ route, navigation }) {
     }
   };
 
-  // On Click
+  // When Submit button clicked
   const clickHandler = () => {
+    setCorrectNum(0);
+    setNumAndLocation(0);
     Keyboard.dismiss();
+
     if (userInput) {
-      if (userInput.length === CELL_COUNT ) {
-        setCorrectNum(0);
-        setNumAndLocation(0);
+      if (userInput.length === CELL_COUNT) {
         let tempUserInput = userInput;
+        //Checking the submission with temp user input
         checkSubmitUpdateState(tempUserInput, randomNumber);
-        //Attepts - game over
+        // Checking attempts and game over
         if (attempts === 1 && userInput !== randomNumber) {
           Alert.alert(`You Lost! The number was: ${randomNumber}`, `:(`, [
             {
@@ -112,15 +111,15 @@ export default function App({ route, navigation }) {
       Alert.alert(`Please enter a ${CELL_COUNT} digit number`);
     }
   };
- 
-  //checking if user input is in range (0-7)
+
+  //Checking if user input is in range (0-7)
   const checkInput = (text) => {
-     if (text % 10 < 8) {
-     setUserInput(text)
-     } else {
-       alert ('Enter numbers between 0-7')
-     }
-  }
+    if (text % 10 < 8) {
+      setUserInput(text);
+    } else {
+      alert("Enter numbers between 0-7");
+    }
+  };
 
   //Reset Game
   const resetGame = () => {
@@ -179,19 +178,19 @@ export default function App({ route, navigation }) {
         <Text>Submit</Text>
       </TouchableOpacity>
       <ScrollView keyboardShouldPersistTaps="handled">
-        <View style={styles.historyText}>
-          {/* <Text>Correct and well places</Text>
-          <Text>Correct and wrongly places</Text> */}
-          </View>
-          <View style={styles.history}>
-            {history
-              .slice(0)
-              .reverse()
-              .map((obj, index) => (
-                <HistoryCell key={index} number={obj.num} result={obj.result} />
-              ))}
-          </View>
-        
+      {/* <View style={styles.resultsTextWrapper}>
+           <Text style={styles.resultsText}>Correct and well placed: </Text>
+           <Text style={styles.resultsText}>Correct and wrongly placed: </Text>
+
+      </View> */}
+        <View style={styles.history}>
+          {history
+            .slice(0)
+            .reverse()
+            .map((obj, index) => (
+              <HistoryCell key={index} number={obj.num} result={obj.result} />
+            ))}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -210,11 +209,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginLeft: 150,
   },
-  historyText: {
-    flexDirection: "row",
-    alignItems: "center",
-
-  },
+  
+  
   history: {
     paddingTop: 10,
     paddingHorizontal: 20,
@@ -224,6 +220,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin: 10,
     textAlign: "center",
+    justifyContent: "space-evenly",
   },
   title: { textAlign: "center", fontSize: 30 },
   codeFieldRoot: {
--- components/HistoryCell.js
+++ components/HistoryCell.js
@@ -2,18 +2,20 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const HistoryCell = (props) => {
-  let num = 1;
-  let numAndLocation = 2;
-  //console.log("props from game", props)
-
   return (
+    <View style={styles.container}>
     <View style={styles.item}>
       <View style={styles.itemLeft}>
         <Text style={styles.itemText}>{props.number}</Text>
       </View>
-      <View style={styles.results}>
-        <Text>correct and well placed: {props.result[0]}</Text>
-        <Text>correct and wrongly placed: {props.result[1]}</Text>
+    </View>
+    <View style={styles.item2}>
+           <Text style={styles.resultsText}>Correct and well placed: </Text>
+           <Text style={styles.resultsNumText}>{props.result[0]}</Text>
+      </View>
+      <View style={styles.item2}>
+           <Text style={styles.resultsText}>Correct and wrongly placed: </Text>
+           <Text style={styles.resultsNumText}>{props.result[1]}</Text>
       </View>
     </View>
   );
@@ -22,28 +24,49 @@ const HistoryCell = (props) => {
 export default HistoryCell;
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
   item: {
     backgroundColor: "#FFFF",
     padding: 15,
     borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 5,
+    width: '45%',
+  },
+  item2: {
+    backgroundColor: "#FFFF",
+    borderRadius: 10,
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 10,
+    justifyContent: "space-evenly",
+    marginBottom: 5,
+    width: '25%',
   },
   itemLeft: {
     flexDirection: "row",
     alignItems: "center",
     flexWrap: "wrap",
   },
-  resultText: {
-      
-  },
   itemText: {
     fontWeight: "bold",
     fontSize: 20,
-    color: "teal",
+    color: "#28A8B3",
     fontFamily: "Avenir",
-    marginLeft: 20,
+  },
+  resultsText:{
+    color: "#357F2F",
+    fontSize: 13,
+    width: 50,
+    lineHeight: 13,
+    textAlign: "center",
+  },
+  resultsNumText:{
+    color: "#FB3E28",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
